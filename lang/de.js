6c684a929830dffe9966e0b65ab618cc7e3dcd5e
dominikmaehl <dominik.maehl@gogol-medien.de>
2018-09-18 13:35:09 +0000

    removed unused translations

--- lang/de.js
+++ lang/de.js
@@ -32,22 +32,12 @@ WBBLANG['de'] = {
 	map: "Karte einbetten",
 	removeFormat:"Formatierung l\u00f6schen",
 
-	modal_social_title: "Social-Media Link einf\u00fcgen",
-	modal_social_text: "Zum Einbinden verwenden Sie bitte die Web-URL und nicht den Einbettcode.",
-	modal_social_infoTextUpperInput: "Es können nur einzelne Beiträge der jeweiligen Plattformen eingebunden werden, nicht jedoch Übersichtsseiten.",
-	modal_social_infoTextUnderInput: "<span>Unterstützte Plattformen:</span><span><ul><li>Facebook</li><li>Instagram</li><li>Twitter</li></ul></span>",
-
-
 	modal_link_title: "Link einf\u00fcgen",
 	modal_link_text: "Text",
 	modal_link_url: "URL",
 	modal_email_text: "Display E-Mail",
 	modal_email_url: "E-Mail",
 	modal_link_tab1: "Insert URL",
-
-	modal_error_url: "Ungultige URL",
-	modal_error_not_embedable: "URL nicht einbettbar. Der Eintrag wird nur verlinkt",
-	modal_error_map: "URLs nicht einbettbar. Der Eintrag wird nur verlinkt. Bitte verwenden Sie den Einbettcode",
 
 	modal_img_title: "Bild hochladen",
 	modal_img_tab1: "URL eingeben",
@@ -57,13 +47,6 @@ WBBLANG['de'] = {
 	add_attach: "Anhang beif\u00fcgen",
 
 	modal_video_text: "Geben Sie die URL des Videos ein",
-	modal_video_infoTextUpperInput: "Es können nur einzelne Videos der jeweiligen Plattformen eingebunden werden, nicht jedoch Playlists, Streams oder Übersichtsseiten.",
-	modal_video_infoTextUnderInput: "<span>Zum Einbinden verwenden Sie bitte die Web-URL und nicht den Einbettungscode.<br>Unterstützte Plattformen:</span><span><ul><li>Youtube</li><li>Vimeo</li></ul></span>",
-
-	modal_map_text: "Geben Sie die Einbettcode der Karte ein",
-	modal_map_infoTextUpperInput: "",
-	modal_map_infoTextUnderInput: "<span>Zum Einbinden verwenden Sie bitte den Einbettcode und nicht die Web-URL.<br>Unterstützte Plattformen:</span><span><ul><li>Google Maps / Google My Maps</li><li>OpenStreetmaps</li></ul></span>",
-
 
 	close: "Schlie\u00dfen",
 	save: "Speichern",
